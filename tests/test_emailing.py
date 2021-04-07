14557fef3b979ce016298c8c0732b0303dcd43fd
Nohossat <nohossat.traore@gmail.com>
2021-04-07 21:18:09 +0200

    fix tests

--- tests/test_emailing.py
+++ tests/test_emailing.py
@@ -4,5 +4,6 @@ from nohossat_cas_pratique.emailing import send_email
 
 
 def test_send_email():
+    print('hello')
     response = send_email("https://neptune.ai", os.getenv('SENDGRID_SENDER'))
     assert response == 202
